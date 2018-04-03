e5f9f1a89cbba2093a76a8bfae05174ba5cc3772
wench <wenchaozhou@qq.com>
2018-04-03 23:55:42 +0800

    NDgraph ok

--- src/graph/NDGraphDemo.java
+++ src/graph/NDGraphDemo.java
@@ -1,6 +1,5 @@
 package graph;
 
-import javax.print.DocFlavor;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,19 +7,48 @@ public class NDGraphDemo {
     public static void main(String[] args) {
         int numarc = 0;
         int numver = 0;
+        int infinity = Integer.MAX_VALUE;
+
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("请输入顶点数和边长数：");
         numver = scan.nextInt();
         numarc = scan.nextInt();
 
         String[] vers = new String[numver] ;
         int[][] arc = new int[numver][numver];
         for(int i=0;i<numver;i++){
+            for(int j=0;j<numver;j++){
+                arc[i][j] = infinity;
+            }
+        }
+
+        System.out.println("请输入顶点名称：");
+        for(int i=0;i<numver;i++){
             vers[i] = scan.next();
+        }
+        System.out.println("请输入"+ numarc+"组 i,j,和权重w:");
+        for(int n=0;n<numarc;n++){
+            int i = scan.nextInt();
+            int j = scan.nextInt();
+            int w = scan.nextInt();
+            arc[i][j] = w;
+            arc[j][i] = w;
         }
         System.out.println("numver..."+numver);
         System.out.println("numarc..."+numarc);
         System.out.println(Arrays.toString(vers));
+        sop(arc);
+    }
+
+    public static void sop(int[][] arrays){
+        for(int i=0;i<arrays.length;i++){
+            for(int j=0;j<arrays[i].length;j++){
+                System.out.print(arrays[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 
 }
